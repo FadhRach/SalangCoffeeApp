1c6f9523fcab8e54e25312dc7188e415c38c4f74
fadhlnrach <fadhlannur007@gmail.com>
2025-09-06 14:43:23 +0700

    Initial Commit

---
+++ SalangCoffee/SalangCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalangCoffeeApp.swift
+//  SalangCoffee
+//
+//  Created by FadhlanNurRach on 06/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SalangCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
